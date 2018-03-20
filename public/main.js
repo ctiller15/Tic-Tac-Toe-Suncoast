5ec1e2ad4545ac4ec2b20a72bf652591a15cca0a
Tiller <ctiller@proforma.com>
2018-03-20 14:11:19 -0400

    update menu to transition between start screen and player choice screen

--- public/main.js
+++ public/main.js
@@ -1,5 +1,25 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const displayMenu = document.querySelector(".display-menu")
+const gamePieceMenu = document.querySelector(".gamePieceMenu");
+
+const startPlayerGame = (event) => {
+  console.log("Starting game vs player!");
+  // First: Hide the menu.
+  console.log(displayMenu);
+  displayMenu.style.display = "none";
+  // Then: Display the gamePiece menu
+  gamePieceMenu.style.display = "block";
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const startComputerGame = (event) => {
+  console.log("Starting game vs computer!");
+}
+
+const choosePlayerPiece = (val) => {
+  console.log(val);
+}
+
+const main = () => {
+  document.querySelector('h1').textContent += '?';
+}
+
+document.addEventListener('DOMContentLoaded', main);
